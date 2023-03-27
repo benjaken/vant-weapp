2901de85295d94f2abb4800479de34b300d29d2d
chenzh <chenzh@szfdc.net.cn>
2023-03-27 10:04:42 +0800

    feat: 优化密码框输入

--- packages/password-input/demo/index.ts
+++ packages/password-input/demo/index.ts
@@ -1,9 +1,18 @@
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2022-06-01 15:23:39
+ * @LastEditors: chenzh chenzh@szfdc.net.cn
+ * @LastEditTime: 2023-03-27 11:06:58
+ * @Description: 
+ * 
+ * Copyright (c) 2023 by 深圳市优房网络有限公司 All Rights Reserved. 
+ */
 import { VantComponent } from '../../common/component';
 import Toast from '../../toast/toast';
 
 VantComponent({
   data: {
-    keyboard: true,
+    keyboard: false,
     value: '123'
   },
   methods: {
@@ -15,6 +24,11 @@ VantComponent({
     closeKeyboard() {
       this.setData({
         keyboard: false
+      })
+    },
+    onFieldChange({ detail }) {
+      this.setData({
+        value: detail
       })
     },
     onInput({ detail }) {
--- packages/password-input/index.ts
+++ packages/password-input/index.ts
@@ -7,37 +7,59 @@ VantComponent({
       value: '',
       observer(val) {
         if (val.length > this.properties.length) {
-          this.setData({
-            value: val.slice(0, this.properties.length),
-          })
+          const value = val.slice(0, this.properties.length);
+          this.setData({ value });
         } else if (val.length === this.properties.length) {
           this.$emit('complete', val);
         }
-      }
+      },
     },
     info: String,
     errorInfo: String,
     length: {
       type: Number,
-      value: 6
+      value: 6,
     },
     gutter: {
       type: Number,
-      value: 0
+      value: 0,
     },
     mask: {
       type: Boolean,
       value: true,
     },
-    focused: Boolean,
     theme: {
       type: String,
-      value: 'square'
+      value: 'square',
     },
+    background: {
+      type: String,
+      value: '#fff',
+    },
+    gutterRadius: {
+      type: String,
+      value: '0',
+    },
+  },
+  data: {
+    focused: false,
   },
   methods: {
     onFocus() {
-      this.$emit('focus')
+      this.$emit('focus');
+      this.setData({ focused: true });
+    },
+    onInput({ detail }) {
+      this.$emit('change', detail.value);
+    },
+    onInputFocus() {
+      this.setData({ focused: true });
+    },
+    onInputBlur() {
+      this.setData({ focused: false });
+    },
+    focus() {
+      this.setData({ focused: true });
     }
-  }
+  },
 });
